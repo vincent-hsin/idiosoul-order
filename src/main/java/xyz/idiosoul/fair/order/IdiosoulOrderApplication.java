840895238659712666be3279eb6f840140da9b78
vincent <xinwei0432@gmail.com>
2020-07-17 16:33:14 +0800

    init maven and spring-boot

---
+++ src/main/java/xyz/idiosoul/fair/order/IdiosoulOrderApplication.java
@@ -0,0 +1,13 @@
+package xyz.idiosoul.fair.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdiosoulOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdiosoulOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/idiosoul/fair/order/IdiosoulOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.idiosoul.fair.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdiosoulOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
