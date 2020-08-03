da056c33b247ab3b899f762a3129633309c43941
vincent <xinwei0432@gmail.com>
2020-08-03 21:25:54 +0800

    clear code

--- src/main/java/xyz/idiosoul/fair/order/domain/model/cart/CartFactory.java
+++ src/main/java/xyz/idiosoul/fair/order/domain/model/cart/CartFactory.java
@@ -3,10 +3,8 @@ package xyz.idiosoul.fair.order.domain.model.cart;
 import org.springframework.stereotype.Component;
 import xyz.idiosoul.fair.order.repository.CartRepository;
 
-import java.util.Optional;
-
 /**
- * 购物车购物组 Factory
+ * 购物车 Factory
  *
  * @author xinw
  */
@@ -19,17 +17,6 @@ public class CartFactory {
     }
 
     public Cart ofCustomer(int customerId) {
-        Optional<Cart> optionalCart = cartRepository.findByCustomerIdAndDeletedFalse(customerId);
-        Cart cart;
-        if (optionalCart.isPresent()) {
-            cart = optionalCart.get();
-        } else {
-            cart = createCart(customerId);
-        }
-        return cart;
-    }
-
-    private Cart createCart(int customerId) {
-        return cartRepository.save(new Cart(customerId));
+        return cartRepository.findByCustomerIdAndDeletedFalse(customerId).orElseGet(() -> cartRepository.save(new Cart(customerId)));
     }
 }
