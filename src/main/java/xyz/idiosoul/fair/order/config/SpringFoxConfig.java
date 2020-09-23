5da5f2030a29b2e43980d2236322599f9789bda5
vincent <xinwei0432@gmail.com>
2020-09-23 19:58:09 +0800

    security with oauth2

--- src/main/java/xyz/idiosoul/fair/order/config/SpringFoxConfig.java
+++ src/main/java/xyz/idiosoul/fair/order/config/SpringFoxConfig.java
@@ -1,20 +1,20 @@
 package xyz.idiosoul.fair.order.config;
 
-import io.swagger.models.auth.In;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.OAuth2Scheme;
 import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.ApiKeyVehicle;
 import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Collections.singletonList;
@@ -24,6 +24,10 @@ import static java.util.Collections.singletonList;
  */
 @Configuration
 public class SpringFoxConfig {
+    private static final String CLIENT_ID = "messaging-client";
+    private static final String CLIENT_SECRET = "secret";
+    private static final String AUTH_SERVER = " http://auth-server:9000/oauth2";
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
@@ -31,35 +35,45 @@ public class SpringFoxConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .securitySchemes(singletonList(apiKey()))
+                .securitySchemes(securitySchemes())
                 .securityContexts(singletonList(securityContext()));
     }
+
     private SecurityContext securityContext() {
         return SecurityContext.builder()
-                .securityReferences(defaultAuth())
+                .securityReferences(Arrays.asList(new SecurityReference("spring_oauth", scopes())))
                 .forPaths(PathSelectors.regex("/api/v1.0/.*"))
                 .build();
-    }
-    List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope
-                = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return singletonList(
-                new SecurityReference(HttpHeaders.AUTHORIZATION, authorizationScopes));
-    }
-
-    private ApiKey apiKey() {
-        return new ApiKey(HttpHeaders.AUTHORIZATION,"apy_key" , In.HEADER.name());
     }
 
     @Bean
     SecurityConfiguration security() {
-        return new SecurityConfiguration(
-                null, null, null,
-                null, // app name
-                "BEARER ", // api key value
-                ApiKeyVehicle.HEADER, "Authorization", ",");
+        return SecurityConfigurationBuilder.builder()
+                .clientId(CLIENT_ID)
+                .clientSecret(CLIENT_SECRET)
+                .scopeSeparator(" ")
+                .useBasicAuthenticationWithAccessCodeGrant(true)
+                .build();
+    }
+
+    private List<SecurityScheme> securitySchemes() {
+        return Arrays.asList(
+                OAuth2Scheme.OAUTH2_AUTHORIZATION_CODE_FLOW_BUILDER
+                        .name("spring_oauth")
+                        .authorizationUrl(AUTH_SERVER + "/authorize")
+                        .tokenUrl(AUTH_SERVER + "/token")
+                        .scopes(Arrays.asList(
+                                new AuthorizationScope("message.read", "Write scope"),
+                                new AuthorizationScope("message.write", "Read scope")))
+                        .build());
+    }
+
+    private AuthorizationScope[] scopes() {
+        AuthorizationScope[] scopes = {
+                new AuthorizationScope("message.read", "Write scope"),
+                new AuthorizationScope("message.write", "Read scope")
+        };
+        return scopes;
     }
 
 }
