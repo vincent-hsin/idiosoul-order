e83b55ab235c4b9b8a7f2b7dac1545745a0cf729
Vincent Hsin <vincent.chi.hsin@gmail.com>
2020-11-19 15:41:54 +0800

    add base test

---
+++ src/test/java/xyz/idiosoul/fair/order/BaseTest.java
@@ -0,0 +1,15 @@
+package xyz.idiosoul.fair.order;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+/**
+ * @author xinw
+ */
+public class BaseTest {
+    @Test
+    public void test(){
+        Optional.of(null);
+    }
+}
